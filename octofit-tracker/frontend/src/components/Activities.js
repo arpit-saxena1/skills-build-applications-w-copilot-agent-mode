7ebeb45c74e74d8437bbeaf66be491b9dd600b52
arpit-saxena1 <arpit.b.saxena@accenture.com>
2025-12-29 10:37:29 +0000

    Add React components for Activities, Leaderboard, Teams, Users, Workouts; setup navigation and API fetch with codespace env

---
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+const Activities = () => {
+  const [activities, setActivities] = useState([]);
+  const codespace = process.env.REACT_APP_CODESPACE_NAME;
+  const endpoint = codespace
+    ? `https://${codespace}-8000.app.github.dev/api/activities/`
+    : '/api/activities/';
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => {
+        const results = data.results || data;
+        setActivities(results);
+        console.log('Activities endpoint:', endpoint);
+        console.log('Fetched activities:', results);
+      });
+  }, [endpoint]);
+
+  return (
+    <div>
+      <h2>Activities</h2>
+      <ul>
+        {activities.map((a, i) => (
+          <li key={a.id || i}>{a.type} - {a.duration} min - {a.date}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Activities;
